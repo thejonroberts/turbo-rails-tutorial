66848cfe1a9ec82abc0e9b524bb8c9fa022c4a82
Jon Roberts <jon.roberts.8@gmail.com>
2024-07-04 21:49:55 -0400

    real time CRUD for quotes

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -7,5 +7,10 @@ class Quote < ApplicationRecord
     # - how to limit access / specify policy for channels?
     # - how to test
   # after_create_commit -> { broadcast_prepend_to "quotes", target: "quotes", partial: "quotes/quote", locals: { quote: self } }
-  after_create_commit -> { broadcast_prepend_to "quotes" }
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit -> { broadcast_replace_later_to "quotes" }
+  # Can't use self.id to broadcast later.
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+  # The above three callbacks are equivalent to the following single line
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 end
