44e372203d4ce033f6dc21452c7e224b45e3f106
Jon Roberts <jon.roberts.8@gmail.com>
2024-07-05 15:19:48 -0400

    turbo for quote line item dates

--- app/controllers/line_item_dates_controller.rb
+++ app/controllers/line_item_dates_controller.rb
@@ -10,7 +10,11 @@ class LineItemDatesController < ApplicationController
     @line_item_date = @quote.line_item_dates.build(line_item_date_params)
 
     if @line_item_date.save
-      redirect_to quote_path(@quote), notice: "Date was successfully created."
+      respond_to do |format|
+        notice = "Date was successfully created."
+        format.html { redirect_to quote_path(@quote), notice: }
+        format.turbo_stream { flash.now[:notice] = notice }
+      end
     else
       render :new, status: :unprocessable_entity
     end
@@ -21,17 +25,25 @@ class LineItemDatesController < ApplicationController
 
   def update
     if @line_item_date.update(line_item_date_params)
-      redirect_to quote_path(@quote), notice: "Date was successfully updated."
+      respond_to do |format|
+        notice = "Date was successfully updated."
+        format.html { redirect_to quote_path(@quote), notice: }
+        format.turbo_stream { flash.now[:notice] = notice }
+      end
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @line_item_date.destroy
+  @line_item_date.destroy
 
-    redirect_to quote_path(@quote), notice: "Date was successfully destroyed."
+  respond_to do |format|
+    notice = "Date was successfully destroyed."
+    format.html { redirect_to quote_path(@quote), notice: }
+    format.turbo_stream { flash.now[:notice] = notice }
   end
+end
 
   private
 
