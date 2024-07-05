5973298b1419ed7767422892558aca669f28e493
Jon Roberts <jon.roberts.8@gmail.com>
2024-07-04 21:39:06 -0400

    broadcast quote creation and display

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,10 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :desc_id_ordered, -> { order(id: :desc) }
+
+  # QUESTIONS:
+    # - how to limit access / specify policy for channels?
+    # - how to test
+  # after_create_commit -> { broadcast_prepend_to "quotes", target: "quotes", partial: "quotes/quote", locals: { quote: self } }
+  after_create_commit -> { broadcast_prepend_to "quotes" }
 end
