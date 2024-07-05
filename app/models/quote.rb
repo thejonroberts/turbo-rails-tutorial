917acb1a54001e741ae6dc41869a22f95a5ed7bd
Jon Roberts <jon.roberts.8@gmail.com>
2024-07-04 23:40:53 -0400

    secure the actioncable channel restrict to company

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -5,14 +5,5 @@ class Quote < ApplicationRecord
 
   scope :desc_id_ordered, -> { order(id: :desc) }
 
-  # QUESTIONS:
-    # - how to limit access / specify policy for channels?
-    # - how to test
-  # after_create_commit -> { broadcast_prepend_to "quotes", target: "quotes", partial: "quotes/quote", locals: { quote: self } }
-  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
-  # after_update_commit -> { broadcast_replace_later_to "quotes" }
-  # Can't use self.id to broadcast later.
-  # after_destroy_commit -> { broadcast_remove_to "quotes" }
-  # The above three callbacks are equivalent to the following single line
-  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+  broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 end
