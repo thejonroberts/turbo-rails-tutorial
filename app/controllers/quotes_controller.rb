b9b32a602a86c3ce148bef101f11a1f20af456fd
Jon Roberts <jon.roberts.8@gmail.com>
2024-07-05 00:35:43 -0400

    flash messages (turbo and regular)

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -19,8 +19,9 @@ class QuotesController < ApplicationController
 
     if @quote.save
     respond_to do |format|
-      format.html { redirect_to quotes_path, notice: "Quote was successfully created." }
-      format.turbo_stream
+      notice = "Quote was successfully created."
+      format.html { redirect_to quotes_path, notice: }
+      format.turbo_stream { flash.now[:notice] = notice }
     end
   else
     render :new, status: :unprocessable_entity
@@ -32,7 +33,11 @@ class QuotesController < ApplicationController
 
   def update
     if @quote.update(quote_params)
-      redirect_to quotes_path, notice: "Quote was successfully updated."
+      respond_to do |format|
+        notice = "Quote was successfully updated."
+        format.html { redirect_to quotes_path, notice: }
+        format.turbo_stream { flash.now[:notice] = notice }
+      end
     else
       render :edit, status: :unprocessable_entity
     end
@@ -41,8 +46,9 @@ class QuotesController < ApplicationController
   def destroy
     @quote.destroy
     respond_to do |format|
-      format.html { redirect_to quotes_path, notice: "Quote was successfully destroyed." }
-      format.turbo_stream
+      notice = "Quote was successfully destroyed."
+      format.html { redirect_to quotes_path, notice: }
+      format.turbo_stream { flash.now[:notice] = notice }
     end
   end
 
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+// disable Turbo on the whole application
+// (also need  <body data-turbo="false"> in views/layouts/application.html.erb)
+// import { Turbo } from "@hotwired/turbo-rails"
+// Turbo.session.drive = false
