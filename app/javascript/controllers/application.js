242fa6a8b8fa76afb742276bbc56c60b6e864bef
Jon Roberts <jon.roberts.8@gmail.com>
2024-07-06 08:37:18 -0400

    better setup for debug env vars

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -2,8 +2,11 @@ import { Application } from "@hotwired/stimulus"
 
 const application = Application.start()
 
-// Configure Stimulus development experience
-application.debug = false
+// set `STIMULUS_DEBUG = 1` in `.env.local` to enable debug mode. see /app/views/layouts/application.html.erb
+if (document.head.querySelector("meta[name=stimulus_debug]")) {
+  application.debug = true
+}
+
 window.Stimulus   = application
 
 export { application }
